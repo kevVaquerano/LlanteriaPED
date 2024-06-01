722b8407aa1dd4fc10a495bc8f3b8acce140f555
kevVaquerano <145158804+kevVaquerano@users.noreply.github.com>
2024-06-01 12:26:05 -0600

    Done

--- Llanteria/obj/Release/net8.0-windows/Login2.AssemblyInfo.cs
+++ Llanteria/obj/Release/net8.0-windows/Login2.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067aa44faa824e3a039d70ef324a58a7e68cb404")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3b372104e7a290f52059e059493f16bd873ddf")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Login2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
